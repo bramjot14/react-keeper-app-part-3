d8cd4106638172a6b06a473c948e0919991a7153
Bramjot Singh <151504705+bramjot14@users.noreply.github.com>
2026-01-06 16:59:33 -0500

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 function Note(props) {
-  function handleClick() {
-    props.onDelete(props.id);
-  }
-
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button
+        onClick={() => {
+          props.onDelete(props.id);
+        }}
+      >
+        DELETE
+      </button>
     </div>
   );
 }
